97d0e78352fe2189dc2711f98649b01643d75c79
Mohammad-Jafiruzzaman <mohammadjafiruzzaman@gmail.com>
2026-02-24 15:48:04 +0600

    learn about cjs module system

---
+++ Backend Part 01/02-node-modules-system/calculate.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+
+module.exports = {
+  add,
+  subtract,
+  multiply,
+};
+
+/*
+  Question: What is Node Module System ?
+  Ans: Node Module System defines how code split, shared, import, export between files so that application remains scalable, maintainable
+ */
---
+++ Backend Part 01/02-node-modules-system/index.js
@@ -0,0 +1,7 @@
+// import calculate
+const { add, subtract, multiply } = require("./calculate.js");
+
+console.log(add(10, 9)); // 19
+console.log(multiply(10, 9)); // 90
+console.log(subtract(10, 9)); // 1
+
